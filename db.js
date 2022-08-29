7853f37df96a592a4b7732065fc5d1831749c5d9
Md. Sohan Talukder Akash <sohan15-1933@diu.edu.bd>
2022-08-29 20:42:58 +0600

    update

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-const todos = require("./data/todos");
-const videos = require("./data/videos");
-const videos2 = require("./data/videos2");
-const transactions = require("./data/transactions");
+const todos = require('./data/todos');
+const videos = require('./data/videos');
+const videos2 = require('./data/videos2');
+const transactions = require('./data/transactions');
+const db_videos = require('./data/db_videos');
 
 module.exports = {
-  todos,
-  videos,
-  videos2,
-  transactions,
-}
-
+	todos,
+	videos,
+	videos2,
+	db_videos,
+	transactions,
+};
